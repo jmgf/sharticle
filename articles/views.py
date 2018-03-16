78cf8551d40f3832141ea8720d5780fb5f4bb5ea
jmgf <joaomgfernandes@hotmail.com>
2018-03-16 15:11:11 +0000

    Adds edit_profile template

--- articles/views.py
+++ articles/views.py
@@ -86,4 +86,26 @@ def profile(request, username):
 # =============================================================================
 
 def edit_profile(request):
-    return HttpResponse("This is the edit_profile view")
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            theUser = request.user
+            # Update user's resume
+            #if request.POST['resume']:
+            #    request.user.resume = request.POST['resume']
+            # Update user's first name
+            if request.POST['first_name']:
+                theUser.first_name = request.POST['first_name']
+            # Update user's last name
+            if request.POST['last_name']:
+                theUser.last_name = request.POST['last_name']
+            # Update user's email
+            if request.POST['email']:
+                theUser.email = request.POST['email']
+            # Save user object to the database
+            theUser.save()
+            return render(request, 'articles/edit_profile.html')
+        else:
+            return render(request, 'articles/edit_profile.html')
+    # In case the user is not authenticated, redirect to login page
+    else:
+        return redirect('articles:login')
