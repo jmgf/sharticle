58e2551a0b113185947a0a3dbfa463ad6ddad1a6
jmgf <joaomgfernandes@hotmail.com>
2018-04-17 17:44:13 +0100

    Updates user model
    Adds last modified date

--- articles/models.py
+++ articles/models.py
@@ -8,11 +8,12 @@ from django.contrib.auth.models import AbstractUser
 
 # Extends Django's default user implementation
 class SharticleUser(AbstractUser):
-    resume = models.CharField(max_length = 512)
+    resume = models.CharField(max_length = 512, null=True, blank=True)
     number_of_articles = models.IntegerField(default = 0)
     number_of_followers = models.IntegerField(default = 0)
     number_of_followees = models.IntegerField(default = 0)
-    profileImagePath = models.CharField(max_length = 512, default = 'http://www.freeiconspng.com/uploads/profile-icon-9.png')
+    profileImagePath = models.CharField(max_length = 512, null=True, blank=True)
+    date_last_modified = models.DateTimeField(auto_now_add = True)
 
     class Meta:
         db_table = 'sharticle_user'
