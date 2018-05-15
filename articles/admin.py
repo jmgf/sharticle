dd1239003bed9c42c9897acbe07877dec3592c6f
jmgf <joaomgfernandes@hotmail.com>
2018-05-15 15:31:52 +0100

    Adds Article model

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from articles.models import SharticleUser
+from articles.models import SharticleUser, Article
 
 # Register your models here.
 
 
 admin.site.register(SharticleUser)
+admin.site.register(Article)
--- articles/models.py
+++ articles/models.py
@@ -10,13 +10,31 @@ from djongo import models as djongo_models
 
 # Extends Django's default user implementation
 class SharticleUser(AbstractUser):
-    resume = models.CharField(max_length = 512, null=True, blank=True)
+    resume = models.CharField(max_length = 512, null = True, blank = True)
     number_of_drafts = models.IntegerField(default = 0)
     number_of_articles = models.IntegerField(default = 0)
     number_of_followers = models.IntegerField(default = 0)
     number_of_followees = models.IntegerField(default = 0)
-    profileImagePath = models.CharField(max_length = 512, null=True, blank=True)
+    profileImagePath = models.CharField(max_length = 512, null = True, blank = True)
     date_last_modified = models.DateTimeField(auto_now_add = True)
 
     class Meta:
         db_table = 'sharticle_user'
+
+
+
+
+class Article(djongo_models.Model):
+    title = djongo_models.CharField(max_length = 128)
+    description = djongo_models.CharField(max_length = 512)
+    author = djongo_models.CharField(max_length = 512)
+    pub_date = djongo_models.DateField(auto_now_add = True)
+    image_path = models.CharField(max_length = 512, null = True, blank = True)
+    date_last_modified = djongo_models.DateTimeField(auto_now_add = True)
+    number_of_comments = djongo_models.IntegerField(default = 0)
+    rating = djongo_models.FloatField(default = 0)
+
+    objects = djongo_models.DjongoManager()
+
+    def __str__(self):
+        return self.author + ' (' + str(self.pub_date) + ')' + ' : "' + self.title  + '"'
