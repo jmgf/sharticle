809b77b04af83c8e651aa69a260a52ce881f6087
jmgf <joaomgfernandes@hotmail.com>
2018-05-21 15:35:59 +0100

    Upgrades views' performance

--- articles/views.py
+++ articles/views.py
@@ -2,20 +2,33 @@ from django.shortcuts import render, redirect
 
 from django.core.cache import cache
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # from django.contrib.auth.models import User
-from .models import SharticleUser
+from .models import SharticleUser, Article
 
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 from django.db import IntegrityError
 
+from django.views.decorators.cache import cache_page, cache_control
+
+from django.views.decorators.http import last_modified
+
+from datetime import datetime
+
+import time
+
+
+
 
 
 # =============================================================================
-# Register view ===============================================================
+# REGISTER view ===============================================================
 # =============================================================================
+
+#@cache_control(max_age=3600, public=True)
+#@cache_page(60*60)
 
 def register(request):
     # Método POST
@@ -26,7 +39,9 @@ def register(request):
         try:
             # Create a user and save it to the database
             user = SharticleUser.objects.create_user(username, username + '@domain.com', password)
-            return HttpResponse("Your account has been created, " + user.username + ".")
+            
+            #return HttpResponse("Your account has been created, " + user.username + ".")
+            return redirect('articles:login')
 
         # In case the user already exists
         except IntegrityError:
@@ -38,9 +53,14 @@ def register(request):
 
 
 
+
+
 # =============================================================================
-# Login view ==================================================================
+# LOGIN view ==================================================================
 # =============================================================================
+
+#@cache_control(max_age=3600, public=True)
+#@cache_page(60*60)
 
 def login(request):
     # Método POST
@@ -52,7 +72,10 @@ def login(request):
         if user is not None:
             # Log in user
             auth_login(request, user)
-            return HttpResponse("You have been successfully logged in, " + request.user.username  + "!")
+            # SUBSTITUIR "request.user.username" POR "user.username" PARA EVITAR ACESSOS À BASE DE DADOS
+            
+            return redirect('articles:edit_profile')
+            # return HttpResponse("You have been successfully logged in, " + request.user.username  + "!")
 
         # In case the user does not exist or the password is wrong
         else:
@@ -63,51 +86,117 @@ def login(request):
 
 
 
+
+
 # =============================================================================
-# Logout view =================================================================
+# LOGOUT view =================================================================
 # =============================================================================
 
 def logout(request):
+    start = time.time()
+
     auth_logout(request)
-    return HttpResponse("You have been logged out!");
+    
+    end = time.time()
+    print(1000*(end - start))
+    
+    return HttpResponse("You have been logged out!")
+
+
 
 
 
 # =============================================================================
-# Profile view ================================================================
+# PROFILE view ================================================================
 # =============================================================================
 
+
+def last_modified_func(request, username):
+    start = time.time()
+    selected_user = SharticleUser.objects.get(username = username)
+    end = time.time()
+    print("SELECT user FROM DATABASE:" + str(1000*(end - start)))
+    return selected_user.date_last_modified
+
+@last_modified(last_modified_func)
 def profile(request, username):
-    selected_user = SharticleUser.objects.get(username = username);
-    return render(request, 'articles/profile.html', context = {'selected_user' : selected_user})
+    try:
+        selected_user = SharticleUser.objects.get(username = username)
+        response = render(request, 'articles/profile.html', context = {'selected_user' : selected_user})
+        response['Cache-Control'] = 'no-cache'
+        return response
+    # In case the user does not exist
+    except SharticleUser.DoesNotExist:
+        return HttpResponse("User does not exist!")
+
+
 
 
 
 # =============================================================================
-# Edit profile view ===========================================================
+# EDIT PROFILE view ===========================================================
 # =============================================================================
+
+def some_func(request):
+    user = request.user
+    if user.is_authenticated:
+        # Return user's profile last modified date
+        return user.date_last_modified
+    else:
+        # Redirect to login page (user is not logged in)
+        return datetime(2018,1,1)
+
+@last_modified(some_func)
 
 def edit_profile(request):
     if request.user.is_authenticated:
         if request.method == "POST":
             theUser = request.user
+
             # Update user's resume
-            if request.POST['resume']:
-                request.user.resume = request.POST['resume']
+            theUser.resume = request.POST['resume']
+            
             # Update user's first name
             if request.POST['first_name']:
                 theUser.first_name = request.POST['first_name']
+            
             # Update user's last name
             if request.POST['last_name']:
                 theUser.last_name = request.POST['last_name']
+            
             # Update user's email
             if request.POST['email']:
                 theUser.email = request.POST['email']
+            
+            # Update user's profile last modified date
+            theUser.date_last_modified = datetime.now()
             # Save user object to the database
             theUser.save()
             return render(request, 'articles/edit_profile.html')
+
         else:
-            return render(request, 'articles/edit_profile.html')
+            start = time.time()
+            t = render(request, 'articles/edit_profile.html')
+            end = time.time()
+            print(1000*(end - start))
+
+            start = time.time()
+            cache.delete('edit_profile_page')
+            #r = cache.get('edit_profile_page')
+            end = time.time()
+            print(1000*(end - start))
+
+            if ("a"!="a"):
+                print("Damn!")
+            else:
+                print ("YEAH!!")
+
+            return t
     # In case the user is not authenticated, redirect to login page
     else:
         return redirect('articles:login')
+
+
+
+
+
