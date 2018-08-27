1dfc973793145b2c73d4e3fb86f906cde84f8cd6
jmgf <joaomgfernandes@hotmail.com>
2018-08-27 22:31:18 +0100

    Removes inline styles
    Removes some comments
    
    Adds 'sharticle.sh' bash script file

---
+++ sharticle.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Script for Sharticle development
+
+# MongoDB
+sudo service mongod start
+
+
+# Memcached
+memcached &
+
+
+# Text editor (VS Code)
+code
+
+# Browser client
+# google-chrome localhost:8000/ &
+
+
+# Celery (task queue)
+celery -A sharticle worker -l info -B &
+
+
+# Application (Sharticle)
+python3 manage.py runserver &
+# gunicorn --bind 0.0.0.0:8000 sharticle.wsgi
+
+
+# Web/application servers
+sudo service gunicorn restart
+sudo service nginx restart
+
+
